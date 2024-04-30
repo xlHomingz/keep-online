5af55f36e9484a195595c74211c418cb6603f526
xlHomingz <76704738+xlHomingz@users.noreply.github.com>
2024-04-30 10:23:20 +0400

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,9 @@ bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
 
+bot.on("ready", () => {
+  console.log("Bot is ready!");
+  bot.editStatus("idle", { name: "Bot is idle" }); // Set status to idle with a custom status message
+});
+
 bot.connect(); // Get the bot to connect to Discord
